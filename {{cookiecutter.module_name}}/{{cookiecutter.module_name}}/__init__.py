548e6915a3b03f11e01518661ea73720a789d951
Tomohiro Nakamura <quickness.net@gmail.com>
2015-12-03 00:19:44 +0900

    done

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 
-VERSION = ({% set lst = cookiecutter.version.split('.') %}{{ lst[0] }}}, {{ lst[1] }}, {{ lst[2] }}, None)
+VERSION = ({% set lst = cookiecutter.version.split('.') %}{{ lst[0] }}, {{ lst[1] }}, {{ lst[2] }}, None)
 
 __version__ = '{0:d}.{1:d}.{2:d}'.format(*VERSION)
 if VERSION[3]:
