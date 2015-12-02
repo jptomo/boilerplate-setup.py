20ca2e893dbb2863a10e7fbad89df36b69254820
Tomohiro Nakamura <quickness.net@gmail.com>
2015-12-03 00:23:36 +0900

    done2

--- {{cookiecutter.module_name}}/setup.py
+++ {{cookiecutter.module_name}}/setup.py
@@ -11,13 +11,13 @@ def _read(fname):
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.pytest_args = []
         if len(sys.argv) == 2:
-            self.pytest_args = ['{{ cookiecutter.module_name }}']
+            self.pytest_args = ["{{ cookiecutter.module_name }}"]
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
@@ -31,20 +31,20 @@ class PyTest(TestCommand):
 
 
 setup(
-    name='{{ cookiecutter.package_name }}',
-    version=__import__('{{ cookiecutter.module_name }}').__version__,
-    author='{{ cookiecutter.author }}',
-    author_email='{{ cookiecutter.author_email }}',
-    url='{{ cookiecutter.url }}',
-    description='',
-    long_description=_read('README.rst'),
+    name="{{ cookiecutter.package_name }}",
+    version=__import__("{{ cookiecutter.module_name }}").__version__,
+    author="{{ cookiecutter.author }}",
+    author_email="{{ cookiecutter.author_email }}",
+    url="{{ cookiecutter.url }}",
+    description="",
+    long_description=_read("README.rst"),
     packages=find_packages(),
-    # install_requires=[''],
-    tests_require=['pytest', 'testfixtures'],
-    cmdclass={'test': PyTest},
+    # install_requires=[""],
+    tests_require=["pytest", "testfixtures"],
+    cmdclass={"test": PyTest},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
     ],
-    license='MIT License',
+    license="MIT License",
 )
